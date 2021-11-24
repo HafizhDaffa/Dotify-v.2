f00f9055423f7a56a25957d7f3cf989c5891a4b5
budisundae <budiswanto46@gmail.com>
2021-11-24 23:59:29 +0700

    gabungan dari redit punya

---
+++ app/src/main/java/com/example/dotify_2/AdapterJadwal.kt
@@ -0,0 +1,59 @@
+package com.example.dotify_2
+
+import android.content.Intent
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.cardview.widget.CardView
+import androidx.core.content.ContextCompat.startActivity
+import androidx.recyclerview.widget.RecyclerView
+import com.google.android.material.button.MaterialButton
+
+class AdapterJadwal(val listJadwal: ArrayList<Jadwal>) : RecyclerView.Adapter<AdapterJadwal.Holder>() {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): Holder {
+        val view: View = LayoutInflater.from(parent.context).inflate(R.layout.jadwal, parent, false)
+        return Holder(view)
+    }
+
+    override fun onBindViewHolder(holder: Holder, position: Int) {
+        val jadwal = listJadwal[position]
+
+        holder.tanggal.text = jadwal.tanggal
+        holder.judul.text = jadwal.judul
+        holder.catatan.text = jadwal.catatan
+        holder.jam.text = jadwal.jam
+
+        holder.note.setOnClickListener {
+            val context = holder.judul.context
+            val intent = Intent(context, UbahJadwal::class.java)
+            context.startActivity(intent)
+        }
+
+        if (!jadwal.isFirst) {
+            holder.tanggal.visibility = View.GONE
+            holder.top.visibility = View.GONE
+        } else {
+            holder.top.visibility = View.INVISIBLE
+        }
+        Log.i("size", listJadwal.size.toString())
+        Log.i("adapter", position.toString())
+
+    }
+
+    override fun getItemCount(): Int {
+        return listJadwal.size
+    }
+
+    inner class Holder(view: View) : RecyclerView.ViewHolder(view) {
+        var tanggal: TextView = view.findViewById(R.id.tanggal)
+        var judul: TextView = view.findViewById(R.id.judul)
+        var catatan: TextView = view.findViewById(R.id.catatan)
+        var jam: TextView = view.findViewById(R.id.jam)
+        var card: CardView = view.findViewById(R.id.card)
+        var top: TextView = view.findViewById(R.id.top)
+        var note: ImageView = view.findViewById(R.id.note)
+    }
+}
---
+++ app/src/main/java/com/example/dotify_2/JadwalSaya.kt
@@ -0,0 +1,46 @@
+package com.example.dotify_2
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.widget.ImageButton
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.google.android.material.button.MaterialButton
+
+class JadwalSaya : AppCompatActivity() {
+    private lateinit var recyclerView: RecyclerView
+    private var list: ArrayList<Jadwal> = arrayListOf()
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_jadwal_saya)
+
+        findViewById<MaterialButton>(R.id.tambah_jadwal).setOnClickListener {
+            val intent = Intent(this, TambahJadwal::class.java)
+            startActivity(intent)
+        }
+
+        findViewById<ImageButton>(R.id.kembali).setOnClickListener {
+            val intent = Intent(this, HalamanUtama::class.java)
+            startActivity(intent)
+        }
+
+        list.addAll(CeritanyaDatabase.listJadwal)
+        list.sortWith(compareBy({it.tanggal.reversed()}, {it.jam}))
+        for (i in list.indices) {
+            if (i < list.size-1) {
+                if (list[i].tanggal == list[i+1].tanggal)
+                    list[i+1].isFirst = false
+            }
+        }
+
+        Log.i("list", list.size.toString())
+
+        recyclerView = findViewById(R.id.recycler_view)
+
+        recyclerView.layoutManager = LinearLayoutManager(this)
+        val adapter = AdapterJadwal(list)
+        recyclerView.adapter = adapter
+    }
+}
